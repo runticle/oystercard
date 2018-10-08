c7e40abb6d6bd16e6b7af1ed86027d5e995866e4
Imogen Misso <imogenmisso@hotmail.co.uk>
2018-10-08 14:32:54 +0100

    allowed users to topup

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,4 +4,11 @@ describe Oystercard do
   it 'has a balance of 0' do
     expect(subject.balance).to eq 0
   end
+
+  describe "#top up" do
+    it "tops up the card by 10" do
+      subject.top_up(10)
+      expect(subject.balance).to eq 10
+    end 
+  end
 end
